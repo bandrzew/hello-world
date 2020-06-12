de4f04aac4ab8a22591a8a30045e0933126538b4
Andrzej Bury <bandrzew@gmail.com>
2020-06-12 08:51:11 +0200

    Added controller and static page

---
+++ src/main/java/com/world/hello/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package com.world.hello.controller;
+
+import com.world.hello.dto.GreetingDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/greetings")
+public class GreetingController {
+
+    @GetMapping
+    public String getGreeting() {
+        return GreetingDto.DEFAULT_GREETING;
+    }
+
+}
---
+++ src/main/java/com/world/hello/dto/GreetingDto.java
@@ -0,0 +1,7 @@
+package com.world.hello.dto;
+
+public class GreetingDto {
+
+    public static final String DEFAULT_GREETING = "Hello World!";
+
+}
